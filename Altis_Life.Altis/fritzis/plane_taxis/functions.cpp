a9d4be66e99a28564b5dfa5605bb178fb4bb93fe
BlubberBerndLP <nuding69@gmail.com>
2016-10-24 11:07:51 +0200

    fix

--- Altis_Life.Altis/fritzis/plane_taxis/functions.cpp
+++ Altis_Life.Altis/fritzis/plane_taxis/functions.cpp
@@ -1,7 +1,7 @@
 class Maverick_PlaneTaxis {
 	tag = "mav_planetaxi";
 	class functions {
-		file = "maverick\plane_taxis";
+		file = "fritzis\plane_taxis";
 		class initializeNPC {};
 		class getClosestAirport {};
 		class initRide {};
